e3691aadbc3118e6bc27f88373a0649ebd03fbb6
JaoVitorMS <joao.vitorms1509@gmail.com>
2026-06-01 17:19:49 -0300

    initial commit backend

---
+++ discovery-server/src/test/java/pucrs/edu/cs/discovery_server/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pucrs.edu.cs.discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
